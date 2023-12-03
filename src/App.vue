caff3c13c0072fbc5e31d90766e09501f7ad1482
DEBASHIR <mironovmir728@gmail.com>
2023-12-03 10:55:11 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <p>{{ text }}</p>
+   <button @click="change1">Менять текст</button>
+   <button @click="change2">Ага</button>
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+export default{
+  data(){
+    return{
+      text: 'Привет',
+    }
+  },
+  methods: {
+    change1: function() {
+      this.text = 'Пока';
+    },
+    change2: function() {
+      this.text = 'Ага'
+    }
+  }
+}
+</script>
